e8685b36438778cef6250eabd500a360ebe73b31
31i0t <eliot@makoski.net>
2021-04-06 18:59:41 +1000

    Skills Question Added

--- src/globalComponents/questionsData.js
+++ src/globalComponents/questionsData.js
@@ -13,30 +13,36 @@ const questions = [
     },
     {
       id: 3,
+      title: 'What are your skills?',
+      description:
+        'I\'m well versed in Python, JavaScript, React, p5.js, Swift (Xcode), HTML, CSS and Git. I\'m also familiar with Photoshop, Autodesk Maya, and Sketch. However, I\'m always learning new technologies, and I love love love experimenting with brand new things.',
+    },
+    {
+      id: 4,
       title: 'What is the best way to reach you?',
       description:
         'Feel free to reach out! Send me an email at this address: eliot@makoski.net',
     },
     {
-      id: 4,
+      id: 5,
       title: 'Do you enjoy working with data structures and algorithms?',
       description:
         'Yes! They\'re very addicting. I often set aside time to keep my skills sharp',
     },
     {
-      id: 5,
+      id: 6,
       title: 'Where are you currently based?',
       description:
         'I\'m currently living in Sydney, Australia and will reside here for the foreseeable future',
     },
     {
-      id: 6,
+      id: 7,
       title: 'What is your ethnicity?',
       description:
         'I\'m African American and European',
     },
     {
-      id: 7,
+      id: 8,
       title: 'Which area of Computer Science are you most interested in?',
       description:
         'Software engineering, and using it as a force for good',
